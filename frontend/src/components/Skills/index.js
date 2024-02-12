124e8d218d701f0e285e15de9e030f670b0b14e9
Asad Ali <asadking066@gmail.com>
2024-02-12 19:03:26 +0500

    changes in skills

--- frontend/src/components/Skills/index.js
+++ frontend/src/components/Skills/index.js
@@ -14,76 +14,76 @@ export default function Index() {
 			</div>
 			<div class="row progress-circle mb-5">
 
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/html.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">HTML</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/css.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">CSS</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/javascript.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">JavaScript</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/php.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">PHP</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/laravel.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">Laravel</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/react.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">React JS</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/nodejs.png`} alt="" style={{width:'100%'}}/>
-						<h2 class="h5 font-weight-bold text-center mb-4">Node/Express JS</h2>
+						<h2 class="h5 font-weight-bold text-center mb-4">Node/Express</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/mongo.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">Mongo DB</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/mysql.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">MySQL DB</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/java.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">Java</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/python.png`} alt="" style={{width:'100%'}}/>
 						<h2 class="h5 font-weight-bold text-center mb-4">Python</h2>
 					</div>
 				</div>
-				<div class="col-lg-3 mb-4" data-aos="zoom-in-up">
+				<div class="col-lg-3 col-sm-6 col-6 mb-4" data-aos="zoom-in-up">
 					<div class="bg-white rounded-lg shadow p-4">
 						<img src={`${process.env.PUBLIC_URL}/Assets/images/Skills/github.png`} alt="" style={{width:'100%'}}/>
-						<h2 class="h5 font-weight-bold text-center mb-4">Git Version Control</h2>
+						<h2 class="h5 font-weight-bold text-center mb-4">GitHub</h2>
 					</div>
 				</div>
 				
